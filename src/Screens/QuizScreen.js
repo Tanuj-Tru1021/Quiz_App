dd0d06cab4e344d49a7ee1855a5c9921856175bb
Tanuj1021 <149355493+Tanuj-Tru1021@users.noreply.github.com>
2024-03-04 21:26:31 +0530

    changes in context

--- src/Screens/QuizScreen.js
+++ src/Screens/QuizScreen.js
@@ -1,15 +1,18 @@
 import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import ratingStar from '../../assets/rating-star.png'
-import OptionsCard from '../Components/OptionsCard';
+// import OptionsCard from '../Components/OptionsCard';
 import { quizScreenStyles } from '../Styles/QuizScreenStyles';
+import { QuestionContext } from '../Constants/ApiContext';
 
 const QuizScreen = ({ navigation }) => {
 
     const [timeLeft, setTimeLeft] = useState(59);
     const [disable, setDisable] = useState(false)
+    const { getCurrentQuestion, getCurrentOptions, handleNextQuestion, currentQuestionIndex, questions } = useContext(QuestionContext)
 
     onPressOption = () => {
+        // handleOptionSelection(index)
         setDisable(true)
     }
 
@@ -26,10 +29,15 @@ const QuizScreen = ({ navigation }) => {
     useEffect(() => {
         startTimer();
         if (timeLeft === 0) {
+            handleNextQuestion()
             setTimeLeft(59)
         }
         return () => clearTimeout(timer);
     });
+
+    useEffect(() => {
+        setDisable(false)
+    }, [currentQuestionIndex])
 
     const data = [
         {
@@ -81,36 +89,72 @@ const QuizScreen = ({ navigation }) => {
                         Question - 1/64
                     </Text>
                     <Text style={quizScreenStyles.questionText}>
-                        Take steps to enhance the activities of Electronics Sector Skill Council in North Eastern States and explore the possibility for opening an electronics skill center in
+                        {getCurrentQuestion()}
                     </Text>
                 </View>
             </View>
             <View style={quizScreenStyles.optionsContainer}>
                 <FlatList
-                    data={data}
+                    data={getCurrentOptions()}
                     keyExtractor={item => item.id}
                     renderItem={({ item }) => (
                         <OptionsCard
-                            option={item.option}
-                            answer={item.correct}
+                            option={item}
+                            // answer={item.correct}
                             onPress={onPressOption}
                             disabled={disable}
+                            questionIndex={currentQuestionIndex}
                         />
                     )}
                     ListFooterComponent={() => (
                         <TouchableOpacity
                             style={{ ...quizScreenStyles.nextButton, backgroundColor: disable ? '#222336' : 'grey', }}
                             disabled={disable ? false : true}
-                            onPress={() => navigation.navigate('Result')}
+                            onPress={() => {
+                                if (currentQuestionIndex + 1 < questions.length) {
+                                    handleNextQuestion()
+                                } else {
+                                    navigation.navigate('Result')
+                                }
+                                setTimeLeft(59)
+                            }}
                         >
                             <Text style={quizScreenStyles.nextText}>
-                                Next
+                                {
+                                    currentQuestionIndex + 1 === questions.length ? 'Submit' : 'Next'
+                                }
                             </Text>
                         </TouchableOpacity>
                     )}
                 />
             </View>
         </View>
+    )
+}
+
+const OptionsCard = ({ disabled, option, answer, onPress, questionIndex }) => {
+
+    const value = answer
+    const [optionSelect, setOptionSelect] = useState(false)
+    const onPressOption = () => {
+        setOptionSelect(true)
+        onPress()
+    }
+    useEffect(() => {
+        setOptionSelect(false)
+    }, [questionIndex])
+
+    const optionColor = optionSelect ? (value ? '#C3EDBF' : 'red') : '#F2F2F2'
+    return (
+        <TouchableOpacity
+            style={{ paddingVertical: 12, paddingLeft: 12, backgroundColor: optionColor, marginBottom: 15, borderRadius: 10 }}
+            onPress={onPressOption}
+            disabled={disabled}
+        >
+            <Text style={{ fontSize: 12, fontWeight: 500, color: '#42003F' }}>
+                {option}
+            </Text>
+        </TouchableOpacity>
     )
 }
 
